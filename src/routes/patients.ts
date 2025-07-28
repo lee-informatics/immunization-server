ef0bd315ae43a1f6c4fcdf58f2faf7ba6be05f1b
user.password <aditya.kotwal.acad@gmail.com>
2025-07-28 10:55:28 -0700

    Fixed an import bug

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import axios, { AxiosResponse } from 'axios';
-import { SERVER_URL } from '../config';
+import { LOCAL_HAPI_SERVER_URL } from '../config';
 
 const router = Router();
 
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   console.log(`[API] GET /api/patients count=${req.query.count || 100}`);
   try {
     const count = req.query.count || 100;
-    const url = `${SERVER_URL}/Patient?_count=${count}`;
+    const url = `${LOCAL_HAPI_SERVER_URL}/Patient?_count=${count}`;
     const response: AxiosResponse = await axios.get(url, { headers: req.headers });
     res.status(response.status).json(response.data);
   } catch (err: any) {

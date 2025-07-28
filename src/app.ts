95caa32393ae05cf9be4025421405bc751edd316
user.password <aditya.kotwal.acad@gmail.com>
2025-07-28 06:45:53 -0700

    Added cache clear API

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ import patientsRouter from './routes/patients';
 import bulkExportRouter from './routes/bulkExport';
 import allergiesRouter from './routes/allergies';
 import staticDataRouter from './routes/staticData';
-import administerRouter from './routes/administration'
+import administerRouter from './routes/administration';
+import { allergyCache } from './services/allergyCache';
+import { exportStatus } from './services/bulkExportService';
 
 const app = express();
 
@@ -19,5 +21,23 @@ app.use('/api/allergies', allergiesRouter);
 app.use('/api', staticDataRouter);
 app.use('/api/administer', administerRouter);
 
+app.delete('/api/cache', (req, res) => {
+  console.log('[API] DELETE /api/cache - Clearing all caches');
+  
+  // Clear allergy cache
+  allergyCache.data = undefined;
+  allergyCache.timestamp = undefined;
+  
+  // Clear export status cache
+  Object.keys(exportStatus).forEach(key => delete exportStatus[key]);
+  
+  res.json({ 
+    message: 'All caches cleared successfully',
+    cleared: {
+      allergyCache: true,
+      exportStatusCache: true
+    }
+  });
+});
 
 export default app; 
